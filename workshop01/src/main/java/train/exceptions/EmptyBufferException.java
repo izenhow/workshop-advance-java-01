8ba7792c9693b28ef972680d412aa73d50cdadf4
Bunyasak Natetawesak <jo_boonyasak@hotmail.com>
2021-02-08 11:39:09 +0700

    Add empty exception classes

---
+++ workshop01/src/main/java/train/exceptions/EmptyBufferException.java
@@ -0,0 +1,5 @@
+package train.exceptions;
+
+public class EmptyBufferException extends Exception {
+    private static final long serialVersionUID = 1L;
+}
---
+++ workshop01/src/main/java/train/exceptions/FullBufferException.java
@@ -0,0 +1,5 @@
+package train.exceptions;
+
+public class FullBufferException extends Exception {
+    private static final long serialVersionUID = 1L;
+}
