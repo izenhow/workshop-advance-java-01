8a0417d91204cb81dbffd60ae9f2bb5c90642dc3
Bunyasak Natetawesak <jo_boonyasak@hotmail.com>
2021-02-09 17:26:06 +0700

    Refactor registration code

--- workshop03/src/main/java/badcode/RegisterBusiness.java
+++ workshop03/src/main/java/badcode/RegisterBusiness.java
@@ -3,25 +3,28 @@ package badcode;
 public class RegisterBusiness {
 
     public Integer register(SpeakerRepository repository, Speaker speaker) {
-        Integer speakerId = 0;
+        validateBusiness(speaker);
+        FeeCalculator.calculateFee(speaker);
+        Integer speakerId = null;
+        try {
+            speakerId = repository.saveSpeaker(speaker);
+        }catch (Exception exception) {
+            throw new SaveSpeakerException("Can't save a speaker.");
+        }
+        return speakerId;
+    }
 
+    private void validateBusiness(Speaker speaker) {
         SpeakerValidationResult validationResult = SpeakerValidator.validateSpeaker(speaker);
         if (validationResult.isPass()) {
             if (EmailValidator.validateEmailDomain(speaker.getEmail())) {
-                FeeCalculator.calculateFee(speaker);
-                try {
-                    speakerId = repository.saveSpeaker(speaker);
-                }catch (Exception exception) {
-                    throw new SaveSpeakerException("Can't save a speaker.");
-                }
+                // ...
             } else {
                 throw new SpeakerDoesntMeetRequirementsException("Speaker doesn't meet our standard rules.");
             }
         } else {
             throw new ArgumentNullException(validationResult.getMessage());
         }
-
-        return speakerId;
     }
 
 }
