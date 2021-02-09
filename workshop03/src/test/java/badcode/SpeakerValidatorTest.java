439e36199f91edd53b385c97e62b2afed47b1c49
Bunyasak Natetawesak <jo_boonyasak@hotmail.com>
2021-02-09 17:12:33 +0700

    Refactor speak validator test

--- workshop03/src/test/java/badcode/SpeakerValidatorTest.java
+++ workshop03/src/test/java/badcode/SpeakerValidatorTest.java
@@ -8,16 +8,16 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class SpeakerValidatorTest {
     @ParameterizedTest
     @CsvSource({
-        "first, last, email, true, null",
+        "first, last, email, true,",
         ", last, email, false, First name is required.",
         ", , email, false, First name is required.",
         "first, , email, false, Last name is required.",
         "first, , , false, Last name is required.",
         "first, last, , false, Email is required.",
     })
-    public void valdiate_speaker(String firstName, String lastName, String email, String isPass, String errMessage) {
+    public void valdiate_speaker(String firstName, String lastName, String email, boolean isPass, String errMessage) {
         Speaker speaker = setupSpeaker(firstName, lastName, email);
-        SpeakerValidationResult expected = setupValidationResult(isPass, errMessage);
+        SpeakerValidationResult expected = new SpeakerValidationResult(isPass, errMessage);
 
         assertEquals(expected, SpeakerValidator.validateSpeaker(speaker));
     }
@@ -28,11 +28,5 @@ public class SpeakerValidatorTest {
         speaker.setLastName(lastName);
         speaker.setEmail(email);
         return speaker;
-    }
-
-    private SpeakerValidationResult setupValidationResult(String isPassString, String errMessage) {
-        boolean isPass = Boolean.valueOf(isPassString);
-        String message = "null".equalsIgnoreCase(errMessage) ? null : errMessage;
-        return new SpeakerValidationResult(isPass, message);
     }
 }
